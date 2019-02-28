f153743a7e2014964df337caffff0d05b2e2d51b
jeffau13 <jeffau13@gmail.com>
2019-02-28 11:58:02 -0500

    route to clear cookies

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if(name){
+    res.render('index', {name});
+    }else{
+        res.redirect('/hello');
+    }
 });
 
 app.get('/cards', (req, res) => {
@@ -19,16 +24,25 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-    res.render('hello', {name: req.cookies.username});
+    const name =req.cookies.username;
+    if(name){
+        res.redirect('/');
+    }else{
+    res.render('hello');
+    }
 });
 app.post('/hello', (req, res) => {
     // console.dir(req.body);
     res.cookie('username',req.body.username);
-    res.render('hello', {name:req.body.username});
-
+    res.redirect('/');
+    
     // res.render('hello');
 });
 
+app.post('/goodbye', (req,res)=>{
+    res.clearCookie('username');
+    res.redirect('/hello');
+});
 
 app.listen(3000);
 console.log('app running on localhost:3000');
