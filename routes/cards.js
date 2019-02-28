9bb0ec70afadc41e731088e6fccfa54e3a3d0ea7
jeffau13 <jeffau13@gmail.com>
2019-02-28 18:50:10 -0500

    /cards redirects to random card question page. Questions and Answers sides have links to respective flipsides

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,37 @@
 const express= require('express');
 const router = express.Router();
+const { data }=require('../data/flashcardData.json');
+const { cards } = data;
 
-router.get('/', (req, res) => {
-    res.locals.prompt = "who is buried in grant's tomb?"
-    res.render('card', { hint: "think about whose tomb it is." });
+router.get('/', (req,res)=>{
+    const range = cards.length;
+    const cardId = Math.floor(Math.random()*range);
+    res.redirect(`/cards/${cardId}?side=question`);
+    
+    // res.redirect(randLink)
+});
 
+router.get('/:id', (req, res) => {
+    const{ side } =req.query;
+    const { id } =req.params;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+    const templateData = {text, id};
+   
+    
+    if(side==='question'){
+        templateData.hint = hint;
+        templateData.otherside = 'answer';
+        templateData.sideToShow = 'Answer';
+    }else if (side==='answer'){
+        templateData.otherside='question';
+        templateData.sideToShow = 'Question';
+    }
+    
+
+    res.render('card',templateData);
+    
+    
 });
 
 module.exports = router;
