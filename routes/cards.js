8a71cbe7029d29de879d8d40253315a8e0b6abbc
jeffau13 <jeffau13@gmail.com>
2019-02-28 19:15:13 -0500

    random flashcards & template changes

--- routes/cards.js
+++ routes/cards.js
@@ -6,18 +6,27 @@ const { cards } = data;
 router.get('/', (req,res)=>{
     const range = cards.length;
     const cardId = Math.floor(Math.random()*range);
-    res.redirect(`/cards/${cardId}?side=question`);
+    res.redirect(`/cards/${cardId}`);
     
     // res.redirect(randLink)
 });
 
 router.get('/:id', (req, res) => {
+   
+    
     const{ side } =req.query;
     const { id } =req.params;
+    
+    if(!side){
+        // console.log(req.query);
+        return res.redirect(`/cards/${id}?side=question`);
+    }
+    const name = req.cookies.username;
     const text = cards[id][side];
     const { hint } = cards[id];
-    const templateData = {text, id};
+    const templateData = {text, id,name};
    
+    
     
     if(side==='question'){
         templateData.hint = hint;
